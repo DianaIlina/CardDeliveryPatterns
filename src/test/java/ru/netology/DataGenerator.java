f4772f787e8117ea833c6b34fd30618f4882af4a
Диана Ильина <diana.1993.30@gmail.com>
2022-06-20 23:54:35 +0100

    Fixing cities and letters

--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -1,25 +1,32 @@
 package ru.netology;
 
 import com.github.javafaker.Faker;
+import lombok.experimental.UtilityClass;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+
+@UtilityClass
 public class DataGenerator {
+    @UtilityClass
     public static class Registration {
         public static String generateDate(int days) {
             return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         }
-        public static DeliveryApplication generateInfo(String Locale) {
+
+        public DeliveryApplication generateInfo(String locale) {
             Faker faker = new Faker(new Locale("ru"));
+
             int days = faker.number().numberBetween(3, 365);
+            String city = ListOfCities.getRandomElement();
 
             return new DeliveryApplication(
                 days,
-                faker.address().cityName(),
+                city,
                 generateDate(days),
-                faker.name().fullName(),
+                faker.name().fullName().replace("ё", "е").replace("Ё", "Е"),
                 faker.phoneNumber().phoneNumber()
             );
         }
